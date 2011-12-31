70f31fcf0e9dcaaf2da215edc1c4ec81844327cb
onjiro <onjiro.mohayhya@gmail.com>
2011-12-31 17:07:32 +0900

    足し算を可能に

--- js/Calculator.js
+++ js/Calculator.js
@@ -19,12 +19,30 @@ Calculator = function() {
         },
         // 計算機への入力を行う
         entry: function(value) {
-            if (evaluatedJustBefore) {
-                incoming = value;
-            } else {
-                incoming += value;
+            if (value.match(/[0-9]/)) {
+                if (evaluatedJustBefore) {
+                    incoming = value;
+                } else {
+                    incoming += value;
+                }
+                evaluatedJustBefore = false;
+            } else if (value.match(/[\+\-\*\/]/)) {
+                if (operator !== "") {
+                    accumulator = eval("return " + accumulator + operator + incoming);
+                } else {
+                    accumulator = incoming;
+                }
+                operator = value;
+                evaluatedJustBefore = true;
+            } else if (value.match(/=/)) {
+                if (operator !== "") {
+                    accumulator = eval(accumulator + operator + incoming).toString();
+                } else {
+                    accumulator = incoming;
+                }
+                evaluatedJustBefore = true;
             }
-            evaluatedJustBefore = false;
+            return this;
         },
     };
 };
--- spec/suites/CalculatorSpec.js
+++ spec/suites/CalculatorSpec.js
@@ -5,14 +5,24 @@ describe("Calculator", function() {
     });
     it("display '1' after entry '1'", function() {
         calc = new Calculator();
-        calc.entry("1");
-        expect(calc.display()).toBe("1");
+        expect(calc.entry("1").display()).toBe("1");
     });
     it("display '10' after entry '1' and '0'", function() {
         calc = new Calculator();
-        calc.entry("1");
-        expect(calc.display()).toBe("1");
-        calc.entry("0");
-        expect(calc.display()).toBe("10");
+        expect(calc.entry("1").display()).toBe("1");
+        expect(calc.entry("0").display()).toBe("10");
+    });
+    
+    it("display '1' after entry '1' and '+'", function() {
+        calc = new Calculator();
+        expect(calc.entry("1").entry("+").display()).toBe("1");
+    });
+    it("display '2' after entry '1', '+' and '2'", function() {
+        calc = new Calculator();
+        expect(calc.entry("1").entry("+").entry("2").display()).toBe("2");
+    });
+    it("display '3' after entry '1', '+', '2' and '='", function() {
+        calc = new Calculator();
+        expect(calc.entry("1").entry("+").entry("2").entry("=").display()).toBe("3");
     });
 });
