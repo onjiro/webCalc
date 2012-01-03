890aebd31adb18f2a2bf9b9dd87719c389b2cee9
onjiro <onjiro.mohayhya@gmail.com>
2012-01-03 20:50:24 +0900

    状態の管理方式を変更

--- js/Calculator.js
+++ js/Calculator.js
@@ -1,10 +1,18 @@
 Calculator = function() {
+    const MODE_ACCUMULATING = 0;
+    const MODE_OPERATOR_SETTED = 1;
+    const MODE_EVALUATED = 2;
+    
+    // 現在の計算機のモード
+    var currentMode = MODE_OPERATOR_SETTED;
     // 評価結果を溜めるスタック
     var accumulator = "0";
     // 数値の入力を受け取るスタック
     var incoming = "0";
     // 現在の計算モード
     var operator = "";
+    // 定数計算機、計算結果の評価時に使用した定数を格納しておく
+    var constantCalculator = "";
     // 直前に評価が行われたかどうか
     var evaluatedJustBefore = true;
     
@@ -12,29 +20,37 @@ Calculator = function() {
         {
             matcher: /[0-9]/,
             responseTo: function(value) {
-                if (evaluatedJustBefore) {
-                    incoming = value;
-                } else {
+                if (currentMode === MODE_ACCUMULATING) {
                     incoming += value;
+                } else {
+                    incoming = value;
                 }
-                evaluatedJustBefore = false;
+                currentMode = MODE_ACCUMULATING;
             }
         },
         {
-            matcher: /[\+\-\*\/=]/, 
+            matcher: /[\+\-\*\/]/, 
             responseTo: function(value) {
-                accumulator = (operator === "") ? incoming: eval(accumulator + operator + incoming).toString();
-                evaluatedJustBefore = true;
-                if (value !== "=") {
+                if (currentMode === MODE_ACCUMULATING) {
+                    calculator.entry("=").entry(value);
+                } else {
                     operator = value;
                 }
+                currentMode = MODE_OPERATOR_SETTED;
+            }
+        },
+        {
+            matcher: "=",
+            responseTo: function(value) {
+                accumulator = (operator === "") ? incoming: eval(accumulator + operator + incoming).toString();
+                currentMode = MODE_EVALUATED;
             }
         },
     ];
-    return {
+    var calculator = {
         // 現在の表示部を返す
         display: function() {
-            return evaluatedJustBefore ? accumulator: incoming; 
+            return currentMode === MODE_ACCUMULATING ? incoming: accumulator;
         },
         // 現在の計算モードを返す
         operator: function() {
@@ -50,4 +66,5 @@ Calculator = function() {
             return this;
         },
     };
+    return calculator;
 };
